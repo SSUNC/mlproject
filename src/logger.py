962e911b92371d3d4f435e7444208480f1f0b941
SSUNC <sunc91@gmail.com>
2025-07-20 16:49:13 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+#packages required
+import logging
+import os
+from datetime import datetime
+
+# To create log files with datetime as names
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
